94c0438c7d8460f407a3ad8c492b68916e672d90
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-07-14 13:12:49 +0300

    Updates

---
+++ precache-manifest.10a7fd5c3b4fa461ee882d6c30dcef61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e34040713ef37107af21f31e75e4b54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7119d4828640f19efba4",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "259efcf119c44bb14090",
+    "url": "./static/js/2.2ded484a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2ded484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7119d4828640f19efba4",
+    "url": "./static/js/main.878bb840.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
